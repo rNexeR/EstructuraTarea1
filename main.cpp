f6c8a0dfe0262ffd005389ddc99a97a09f6c4c6a
Turupawn <turupawn@userr>
2014-07-15 23:59:35 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+// queue::push/pop
+#include "Evaluador.h"
+#include <iostream>       // std::cin, std::cout
+#include <queue>          // std::queue
+using namespace std;
+
+string getPrimerElemento(queue<string> mi_cola)
+{
+    return "";
+}
+
+//devuelve la cantidad de elementos de "mi_cola"
+int getTamano(queue<string> mi_cola)
+{
+    return -1;
+}
+
+//devuelve true si "str" esta dentro de "mi_cola" de lo contrario devuelve false
+bool existeEnCola(queue<string> mi_cola, string str)
+{
+    return false;
+}
+
+//devuelve el elemento mayor de "mi_cola"
+int getMayor(queue<int> mi_cola)
+{
+    return -1;
+}
+
+//devuelve el elemento menor de "mi_cola"
+int getMenor(queue<int> mi_cola)
+{
+    return -1;
+}
+
+float getPromedio(queue<float> mi_cola)
+{
+    return -1.0f;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
