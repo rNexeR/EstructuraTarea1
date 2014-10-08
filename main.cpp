ae502a5c56203e53c8edea2550a4f290d6591904
Turupawn <ahmed.hn.43@gmail.com>
2014-10-07 22:33:21 -0600

    cambiando de cola a pila

--- main.cpp
+++ main.cpp
@@ -5,38 +5,38 @@
 using namespace std;
 
 
-//devuelve el primer elemento de "mi_cola"
-string getTope(stack<string> mi_cola)
+//devuelve el primer elemento de "mi_pila"
+string getTope(stack<string> mi_pila)
 {
     return "";
 }
 
-//devuelve la cantidad de elementos de "mi_cola"
-int getTamano(stack<string> mi_cola)
+//devuelve la cantidad de elementos de "mi_pila"
+int getTamano(stack<string> mi_pila)
 {
     return -1;
 }
 
-//devuelve true si "str" esta dentro de "mi_cola" de lo contrario devuelve false
-bool existeEnCola(stack<string> mi_cola, string str)
+//devuelve true si "str" esta dentro de "mi_pila" de lo contrario devuelve false
+bool existeEnCola(stack<string> mi_pila, string str)
 {
     return false;
 }
 
-//devuelve el elemento mayor de "mi_cola"
-int getMayor(stack<int> mi_cola)
+//devuelve el elemento mayor de "mi_pila"
+int getMayor(stack<int> mi_pila)
 {
     return -1;
 }
 
-//devuelve el elemento menor de "mi_cola"
-int getMenor(stack<int> mi_cola)
+//devuelve el elemento menor de "mi_pila"
+int getMenor(stack<int> mi_pila)
 {
     return -1;
 }
 
-//devuelve el promedio de todos los elementos de "mi_cola"
-float getPromedio(stack<float> mi_cola)
+//devuelve el promedio de todos los elementos de "mi_pila"
+float getPromedio(stack<float> mi_pila)
 {
     return -1;
 }
