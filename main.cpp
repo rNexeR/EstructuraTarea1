4f02bc6b2baebeefcb505faeabdeb32f5567972d
rnexer <rnexer@gmail.com>
2015-03-26 09:56:07 -0600

    s

--- main.cpp
+++ main.cpp
@@ -8,37 +8,64 @@ using namespace std;
 //devuelve el primer elemento de "mi_pila"
 string getTope(stack<string> mi_pila)
 {
-    return "";
+    return mi_pila.top();
 }
 
 //devuelve la cantidad de elementos de "mi_pila"
 int getTamano(stack<string> mi_pila)
 {
-    return -1;
+    return mi_pila.size();
 }
 
 //devuelve true si "str" esta dentro de "mi_pila" de lo contrario devuelve false
 bool existeEnPila(stack<string> mi_pila, string str)
 {
+    for(int x = 0; x<mi_pila.size(); x++)
+        if (mi_pila.top()==str)
+            return true;
+        else
+            mi_pila.pop();
     return false;
 }
 
 //devuelve el elemento mayor de "mi_pila"
 int getMayor(stack<int> mi_pila)
 {
-    return -1;
+    int mayor = mi_pila.top();
+    int y = mi_pila.size();
+    for(int x = 0; x<y; x++){
+        if (mi_pila.top()>mayor)
+            mayor = mi_pila.top();
+        mi_pila.pop();
+    }
+    return mayor;
 }
 
 //devuelve el elemento menor de "mi_pila"
 int getMenor(stack<int> mi_pila)
 {
-    return -1;
+    int menor = mi_pila.top();
+    int y = mi_pila.size();
+    for(int x = 0; x<y; x++){
+        if (mi_pila.top()<menor)
+            menor = mi_pila.top();
+        mi_pila.pop();
+    }
+    return menor;
 }
 
 //devuelve la cantidad de veces que se encuentra la letra 'a' minuscula en "mi_pila"
 int contarLetraA(stack<char> mi_pila)
 {
-    return -1;
+    int y = mi_pila.size();
+    int cant = 0;
+
+    for(int x = 0; x<y; x++){
+        if(mi_pila.top()=='a')
+            cant++;
+        mi_pila.pop();
+    }
+    return cant;
 }
 
 int main ()
